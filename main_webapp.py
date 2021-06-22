1aa0371702c946012f60e4e7d0e59015a2707945
sanchitjain2003 <sanchitjain2003@gmail.com>
2021-06-22 09:33:26 +0530

    Fixing errors and bugs to handle multiple inputs

--- main_webapp.py
+++ main_webapp.py
@@ -6,3 +6,4 @@ input("What's your name?")
 select("Select food", ['Orange', 'Apple'])
 checkbox("Are your okay?", options=["I'm okay."])
 radio("What do you like to do?", options=['Eat', 'Sleep', 'Study'])
+
--- main_with_excel.py
+++ main_with_excel.py
@@ -1,5 +1,6 @@
 import requests
 import xlsxwriter
+import fire
 
 # main driver
 main_flag = True
@@ -26,28 +27,34 @@ def create_barcode(data, dash_index,row_num, col_num):
 
     else:
         temp = col_num
-        worksheet.write(row_num, temp, inp[:dash_index[0]])
-        worksheet.write(row_num, temp+1, inp[dash_index[0]+1:dash_index[3]])
-        worksheet.write(row_num, temp+2, inp[dash_index[3]+1:])
+        worksheet.write(row_num, temp, data[:dash_index[0]])
+        worksheet.write(row_num, temp+1, data[dash_index[0]+1:dash_index[3]])
+        worksheet.write(row_num, temp+2, data[dash_index[3]+1:])
         worksheet.write(row_num, temp+3, url)
         row_num += 1
-        col_num += 1
+        col_num = 0
         return row_num, col_num
 
 def string_format(data):
+    dash_index = []
     for i in range(0,len(data)):
         if data[i] == '-':
             dash_index.append(i)
+            #print(dash_index)
     return dash_index
 
 
-while main_flag:
-    inp = input()
-    if inp.lower() == "exit":
-        print("Program exiting")
-        workbook.close()
-        exit()
-    
-    else:
-        dash_index = string_format(inp)
-        row_num, col_num = create_barcode(inp, dash_index, row_num, col_num)
+def main(row_num, col_num):
+    while main_flag:    
+        inp = input()
+        if inp.lower() == "exit":
+            print("Program exiting")
+            workbook.close()
+            exit()
+        
+        else:
+            dash_index = string_format(inp)
+            row_num, col_num = create_barcode(inp, dash_index, row_num, col_num)
+
+if __name__ == '__main__':
+  fire.Fire(main(1,0))
